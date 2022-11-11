be98109144226f167d34bd007d6a54e9cca79d5b
Leopold Johannes Meinel <leo@meinel.dev>
2022-11-11 04:54:37 +0100

    Make consts less messy

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,14 @@ pub const WINDOW_WIDTH: f32 = 1280.0;
 pub const WINDOW_HEIGHT: f32 = 720.0;
 pub const WINDOW_TITLE: &str = "Bevy Tower Defense";
 pub const WINDOW_RESIZABLE: bool = true;
-pub const CAMERA0: ((f32, f32, f32), (Vec3, Vec3)) = ((-2.0, 2.5, 5.0), (Vec3::ZERO, Vec3::Y));
-pub const PLANE0: (f32, (f32, f32, f32)) = (5.0, (0.3, 0.5, 0.3));
-pub const CUBE0: (f32, (f32, f32, f32), (f32, f32, f32)) =
-    (1.0, (0.67, 0.84, 0.92), (0.0, 0.5, 0.0));
+pub const CAMERA0: (Transform, (Vec3, Vec3)) =
+    (Transform::from_xyz(-2.0, 2.5, 5.0), (Vec3::ZERO, Vec3::Y));
+pub const PLANE0: (f32, Color) = (5.0, Color::rgb(0.3, 0.5, 0.3));
+pub const CUBE0: (f32, Color, Transform) = (
+    1.0,
+    Color::rgb(0.67, 0.84, 0.92),
+    Transform::from_xyz(0.0, 0.5, 0.0),
+);
 
 fn main() {
     App::new()
@@ -38,8 +42,7 @@ fn main() {
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn_bundle(Camera3dBundle {
-        transform: Transform::from_xyz(CAMERA0.0 .0, CAMERA0.0 .1, CAMERA0.0 .2)
-            .looking_at(CAMERA0.1 .0, CAMERA0.1 .1),
+        transform: CAMERA0.0.looking_at(CAMERA0.1 .0, CAMERA0.1 .1),
         ..default()
     });
 }
@@ -51,13 +54,13 @@ fn spawn_basic_scene(
 ) {
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Plane { size: PLANE0.0 })),
-        material: materials.add(Color::rgb(PLANE0.1 .0, PLANE0.1 .1, PLANE0.1 .2).into()),
+        material: materials.add(PLANE0.1.into()),
         ..default()
     });
     commands.spawn_bundle(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::Cube { size: CUBE0.0 })),
-        material: materials.add(Color::rgb(CUBE0.1 .0, CUBE0.1 .1, CUBE0.1 .2).into()),
-        transform: Transform::from_xyz(CUBE0.2 .0, CUBE0.2 .1, CUBE0.2 .2),
+        material: materials.add(CUBE0.1.into()),
+        transform: CUBE0.2,
         ..default()
     });
 }
